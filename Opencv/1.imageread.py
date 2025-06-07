4e12cabbdd49b83263e1e42630ef03e2abd0bc1f
Halfgods <justinroy9702@gmail.com>
2025-06-07 18:00:47 +0530

    Add files via upload

---
+++ Opencv/1.imageread.py
@@ -0,0 +1,4 @@
+import cv2 as cv
+Matrix = cv.imread('pictures/4.png')
+cv.imshow('3.png' , Matrix)
+cv.waitKey(0)
---
+++ Opencv/2.videoread.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+capture = cv.VideoCapture('videos/video1.mp4')  
+'''WebCam'''
+while True:
+    isTrue , frame = capture.read()
+    flip = cv.flip(frame,1)
+    cv.imshow("Video" , flip)
+    if cv.waitKey(20) & 0xFF == ord('q'):
+        break
+    
+capture.release()
+cv.destroyAllWindows()
